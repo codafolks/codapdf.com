6677ab2967a7a47556ec8707f9603cc7a37d7914
Falconiere R. Barbosa <falconieer@gmail.com>
2024-11-04 19:50:17 -0300

    fix redirect

--- apps/app.codapdf.com/src/app/api/auth/callback/github/route.ts
+++ apps/app.codapdf.com/src/app/api/auth/callback/github/route.ts
@@ -3,13 +3,28 @@ import { authByGithub } from "@/server/actions/auth/authByGithub";
 import { captureException } from "@sentry/nextjs";
 import { NextRequest } from "next/server";
 import { env } from "process";
+
+// NOTE: This is a workaround to redirect the user to the dashboard after google login. For some reason, the Response.redirect() function is not working.
+const reload = `
+  <h3>Redirecting...</h3>
+  <script>
+    setTimeout(() => {
+      window.location.href = "${ROUTES.PRIVATE.DASHBOARD.path}";
+    }, 1000);
+  </script>
+`;
+
 export const GET = async (req: NextRequest) => {
   try {
     const url = new URL(req.url);
     const code = url.searchParams.get("code");
     await authByGithub(code);
-    const redirectUrl = `${env.APP_DOMAIN}/${ROUTES.PRIVATE.DASHBOARD.path}`;
-    return Response.redirect(redirectUrl);
+    return new Response(reload, {
+      status: 200,
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
   } catch (error) {
     captureException(error);
     const redirectUrl = `${env.APP_DOMAIN}/${ROUTES.AUTH.LOGIN.path}`;
