b874ee6fc3503126e2971ac0f8a28d04e101749f
Falconiere R. Barbosa <falconieer@gmail.com>
2024-11-06 22:40:35 -0300

    improve page loading

--- apps/app.codapdf.com/src/middleware.ts
+++ apps/app.codapdf.com/src/middleware.ts
@@ -4,6 +4,7 @@ import type { NextRequest } from "next/server";
 export const middleware = async (request: NextRequest) => {
   return await authMiddleware({ request });
 };
+
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
