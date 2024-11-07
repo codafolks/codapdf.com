25803ecdc4727b9c6a24538339794e9284a02ba3
Falconiere R. Barbosa <falconieer@gmail.com>
2024-11-07 07:09:21 -0300

    fix responsiveness on home page

--- apps/app.codapdf.com/src/app/(marketing)/_components/Header.tsx
+++ apps/app.codapdf.com/src/app/(marketing)/_components/Header.tsx
@@ -2,28 +2,45 @@
 import { ROUTES } from "@/app/routes";
 import { AppLogo } from "@/client/components/app/AppLogo";
 import { ButtonUpdateTheme } from "@/client/components/app/ButtonUpdateTheme";
+import { Button } from "@/client/components/ui/button";
+import { useIsMobile } from "@/client/hooks/useIsMobile";
+import { cn } from "@/client/lib/utils";
+import { AlignJustify } from "lucide-react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 
-export const scrollSmoothTo = (id: string) => {
-  if (typeof window === "undefined" || typeof document === "undefined") return;
-  const element = document.getElementById(id);
-  const container = document.getElementById("marketing-layout");
-  if (element && container) {
-    // Scroll to the top of the element with margin top of 20px
-    container.scrollTo({
-      top: element.offsetTop - 40,
-      behavior: "smooth",
-    });
-  }
-};
 export const Header = () => {
+  const isMobile = useIsMobile();
   const router = useRouter();
   const pathname = usePathname();
+  const scrollSmoothTo = (id: string) => {
+    if (typeof window === "undefined" || typeof document === "undefined") return;
+    const element = document.getElementById(id);
+    const container = document.getElementById("marketing-layout");
+    if (element && container) {
+      // Scroll to the top of the element with margin top of 20px
+      container.scrollTo({
+        top: element.offsetTop - 40,
+        behavior: "smooth",
+      });
+    }
+    if (isMobile) {
+      document.getElementById("navigation")?.classList.toggle("hidden");
+    }
+  };
   return (
-    <header className="fixed top-0 left-0 z-10 flex h-full flex-col border-b bg-background p-4 text-foreground md:sticky md:w-full md:flex-col md:items-center lg:px-6">
+    <header className="sticky top-0 left-0 z-10 flex w-full flex-col border-b bg-background px-4 py-2 text-foreground md:flex-row md:items-center lg:px-6">
+      <Button
+        className="self-end md:hidden"
+        variant="ghost"
+        onClick={() => {
+          document.getElementById("navigation")?.classList.toggle("hidden");
+        }}
+      >
+        <AlignJustify className="h-6 w-6" />
+      </Button>
       <Link
-        className="flex items-center justify-center "
+        className="hidden items-center justify-center p-2 md:flex"
         href="#home"
         onClick={(e) => {
           e.preventDefault();
@@ -37,45 +54,56 @@ export const Header = () => {
       >
         <AppLogo className="h-4 md:h-6" />
       </Link>
-      <nav className="ml-auto flex flex-col gap-4 sm:gap-6 ">
-        <Link
-          className="font-medium text-sm underline-offset-4 hover:text-gray-300 hover:underline"
-          href="#features"
-          scroll={false}
-          onClick={(e) => {
-            e.preventDefault();
-            scrollSmoothTo("features");
-          }}
-        >
-          Features
-        </Link>
-        <Link
-          className="font-medium text-sm underline-offset-4 hover:text-gray-300 hover:underline"
-          href="#pricing"
-          scroll={false}
-          onClick={(e) => {
-            e.preventDefault();
-            scrollSmoothTo("pricing");
-          }}
-        >
-          Pricing
-        </Link>
-        <Link className="font-medium text-sm underline-offset-4 hover:text-gray-300 hover:underline" href={ROUTES.PUBLIC.DOCS.path} scroll={false}>
-          Docs
-        </Link>
-        <Link
-          className="font-medium text-sm underline-offset-4 hover:text-gray-300 hover:underline"
-          href="#contact"
-          scroll={false}
-          onClick={(e) => {
-            e.preventDefault();
-            scrollSmoothTo("contact");
-          }}
-        >
-          Contact
-        </Link>
+      <nav
+        className={cn("mt-4 hidden flex-col items-center p-2 md:mt-auto md:ml-auto md:flex-1 md:flex-row md:justify-end", {
+          flex: isMobile,
+          "md:flex": !isMobile,
+        })}
+        id="navigation"
+      >
+        <Button variant="link" asChild>
+          <Link
+            href="#features"
+            scroll={false}
+            onClick={(e) => {
+              e.preventDefault();
+              scrollSmoothTo("features");
+            }}
+          >
+            Features
+          </Link>
+        </Button>
+        <Button variant="link" asChild>
+          <Link
+            href="#pricing"
+            scroll={false}
+            onClick={(e) => {
+              e.preventDefault();
+              scrollSmoothTo("pricing");
+            }}
+          >
+            Pricing
+          </Link>
+        </Button>
+        <Button variant="link" asChild>
+          <Link href={ROUTES.PUBLIC.DOCS.path} scroll={false}>
+            Docs
+          </Link>
+        </Button>
+        <Button variant="link" asChild>
+          <Link
+            href="#contact"
+            scroll={false}
+            onClick={(e) => {
+              e.preventDefault();
+              scrollSmoothTo("contact");
+            }}
+          >
+            Contact
+          </Link>
+        </Button>
+        <ButtonUpdateTheme />
       </nav>
-      <ButtonUpdateTheme />
     </header>
   );
 };
