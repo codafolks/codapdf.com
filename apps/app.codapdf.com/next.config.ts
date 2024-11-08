79e73207c917f866007aedd226c223420aa31da6
Falconiere R. Barbosa <falconieer@gmail.com>
2024-11-08 14:22:05 -0300

    implement cache

--- apps/app.codapdf.com/next.config.ts
+++ apps/app.codapdf.com/next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: Config = {
   output: "standalone",
   serverExternalPackages: ["pino", "pino-pretty"],
   experimental: {
+    optimizeCss: true,
+    optimisticClientCache: true,
+    optimizeServerReact: true,
+    nextScriptWorkers: true,
+    gzipSize: true,
     staleTimes: {
       dynamic: 60 * 60 * 24, // 24 hours
       static: 60 * 60 * 24, // 24 hours
