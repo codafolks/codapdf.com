c3347a9039f0ed194caea79d53ec14fb2cf958f7
Falconiere R. Barbosa <falconieer@gmail.com>
2024-11-11 23:55:42 -0300

    updating apikey

--- apps/app.codapdf.com/src/app/api/auth/validate-token/route.ts
+++ apps/app.codapdf.com/src/app/api/auth/validate-token/route.ts
@@ -1,8 +1,11 @@
 import { getAuthorizationTokenFromHeader } from "@/server/actions/auth/getAuthorizationTokenFromHeader";
 import { verifyJwt } from "@/server/actions/auth/verifyJwt";
+import { db } from "@/server/database";
+import { apiKeys } from "@/server/database/schemas/apiKeys";
 
 import { captureException } from "@/utils/captureException";
 import { checkUserLicense } from "@/utils/checkUserLicense";
+import { eq } from "drizzle-orm";
 import { type NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
@@ -11,6 +14,25 @@ export const POST = async (req: NextRequest) => {
     if (!token) {
       return new Response("Unauthorized", { status: 401 });
     }
+    const apiKey = await db.query.apiKeys.findFirst({
+      where: eq(apiKeys.apiKey, token),
+      with: {
+        user: {
+          with: {
+            profile: true,
+          },
+        },
+      },
+    });
+
+    if (apiKey) {
+      const license = await checkUserLicense(apiKey.user.profile.id);
+      if (!license) {
+        return new Response("Unauthorized", { status: 401 });
+      }
+      return NextResponse.json(license);
+    }
+
     const isValid = await verifyJwt(token);
     if (!isValid) {
       return new Response("Unauthorized", { status: 401 });
